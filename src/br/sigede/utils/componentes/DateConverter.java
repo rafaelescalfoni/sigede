c3538e671edf0096133a24f6fa53548941e1b44a
Rafael Escalfoni <rafaelescalfoni@gmail.com>
2015-11-09 11:23:05 -0200

    Incluindo conversor de datas para evitar problemas de formatação. O formato aceito pelo conversor é dd/MM/yyyy hh:mm Caso queira mudar, altere a classe DateConverter.java, em br.sigede.utils.componentes

---
+++ src/br/sigede/utils/componentes/DateConverter.java
@@ -0,0 +1,35 @@
+package br.sigede.utils.componentes;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+import br.com.caelum.vraptor.Convert;
+import br.com.caelum.vraptor.Converter;
+import br.com.caelum.vraptor.ioc.ApplicationScoped;
+
+@Convert(Date.class)
+@ApplicationScoped
+public class DateConverter implements Converter<Date> {
+
+	@Override
+	public Date convert(String value, Class<? extends Date> arg1, ResourceBundle msg) {
+		if(value == null || value.isEmpty()) {
+			return null;
+		}
+		
+		try {  
+            String dataEmTexto = value;  
+            Date date = new SimpleDateFormat("dd/MM/yyyy hh:mm").parse(dataEmTexto);  
+            return date;  
+        } catch (ParseException e) {  
+            // TODO Auto-generated catch block  
+            e.printStackTrace();  
+            return null;  
+        }  
+		
+	}
+
+}
